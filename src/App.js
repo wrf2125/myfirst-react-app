0fbe17082676f727204b3cbac44113d57de6b91f
RAJU <gkraju3exp@gmail.com>
2020-04-25 00:57:52 +0530

    udemy Section2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person";
+import Shape from "./Shape";
 function App() {
   return (
     <div className="App">
-      <Person name="RAJU" />
+      <Shape name="square" length="10" breadth="20" />
+      <br />
+      <br />
+      <Shape name="circle" length="100" breadth="200" />
     </div>
   );
 }
